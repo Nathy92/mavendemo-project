0b6f85d04b40798668011493a69f9bd05d02fcdc
Nathy92 <201106639@student.uj.ac.za>
2020-03-10 17:28:44 +0200

    initial version

---
+++ mavendemo/src/main/java/com/learn/mavendemo/App.java
@@ -0,0 +1,13 @@
+package com.learn.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
